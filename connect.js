768885a7f5a3d6ef644de8f7d370f41c4fc79e6c
thiagosantosleite <thiagosantosleite@yahoo.com.br>
2016-05-31 10:41:29 -0300

    teste

--- connect.js
+++ connect.js
@@ -1,6 +1,6 @@
 var mongodb = require('mongodb');
 //var uri = "mongodb://root:mongodb@192.168.217.41:27001,192.168.217.41:27002,192.168.217.41:27003/movies_c1?authSource=admin&readPreference=secondary&poolSize=30&auto_reconnect=true";
-var uri = 'mongodb://@54.183.185.216r:27020/testedb?authSource=admin&poolSize=30&auto_reconnect=true';
+var uri = 'mongodb://@54.183.185.216:27020/testedb?authSource=admin&poolSize=30&auto_reconnect=true';
 
 module.exports = function(callback) {
   mongodb.MongoClient.connect(uri, callback);
--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var assert = require('assert');
 
 connect(function(error, conn) {
   if (error) {
-    return done(error);
+    console.log(error);
   }
   db = conn;
 
@@ -13,9 +13,11 @@ connect(function(error, conn) {
   var doc = { title: 'Rogue One', year: 2016, director: 'Gareth Edwards' };
 
   dbInterface.insert(db, doc, function(error) {
+    sleep(1000);
     db.collection('movies').count({ title: 'Rogue One' }, function(error, c) {
       assert.ifError(error);
       assert.equal(c, 1);
     });
+    sleep(1000);
   });
 });
